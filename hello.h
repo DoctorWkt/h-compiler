01a733acade7a22ca779801c2a8d8b7128955a7a
Warren Toomey <wkt@tuhs.org>
2016-04-29 03:13:58 +0000

    *** empty log message ***

--- hello.h
+++ hello.h
@@ -1,18 +1,6 @@
 int x = 2;
-int y = 32;
-int z = 8;
-int a;
-int b;
-
-int fred(int p, int q, int r)
-{
-  p= 3;
-  x= p + q + r;
-}
-
 
 void main()
 {
-  fred(x,y,z);
-  x = x + 3;
+  printn(x);
 }
