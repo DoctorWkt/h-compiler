926bf71cab8e646d952e7f0f197e682756c2fe36
Warren Toomey <wkt@tuhs.org>
2016-04-29 12:38:30 +0000

    *** empty log message ***

--- hello.h
+++ hello.h
@@ -1,16 +1,15 @@
-int fred(int a, int b)
-{
-  return(a + b);
-}
-
 void main()
 {
   int x;
-  x= fred(4, 5);
-  printn(x);
-  x= fred(2, 6);
-  printn(x);
-  x= fred(1, 1);
-  printn(x);
+  int y;
+  int z;
+
+  for (x=1; x != 11; x++) {
+    for (y=1; y != 11; y++) {
+      z= x * y;
+      printn(z);
+    }
+    if (x==5) break;
+  }
 }
 
