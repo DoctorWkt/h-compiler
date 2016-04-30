037a80a53367e058bdaf17a759e7de9425aa855c
Warren Toomey <wkt@tuhs.org>
2016-04-30 23:06:45 +1000

    Speed up a bit by not calling addr() all the time. Also check that octal constants work OK.

--- hello.h
+++ hello.h
@@ -1,6 +1,17 @@
 char str1[]= "This is my first string\n";
 void puts(char *c);
 
+void main()
+{
+  int x;
+
+  puts(str1);
+  puts("and this should get printed, too.\n");
+
+  for (x=00; x != 020; x++)
+    printn(x);
+}
+
 void puts(char *c)
 {
   while (*c != '\0') {
@@ -8,8 +19,3 @@ void puts(char *c)
   }
 }
 
-void main()
-{
-  puts(str1);
-  puts("and this should get printed, too.\n");
-}
